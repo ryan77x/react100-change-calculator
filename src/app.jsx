fd7160c68f12ec46a4c93dfa65157e836e6d6051
Ryan Liang <ryan7x7x@gmail.com>
2019-06-26 01:35:25 -0700

    added title of main page

--- src/app.jsx
+++ src/app.jsx
@@ -6,6 +6,16 @@ class App extends Component {
   }
 
   render() {
+    return(
+      <div className='container'>
+        <h3>Change Calculator</h3>
+        <hr/>
+        <div className="row">
+        <div className="col-sm-4" >.col-sm-4</div>
+        <div className="col-sm-8" >.col-sm-8</div>
+        </div>
+      </div>  
+    );
   }
 }
 
